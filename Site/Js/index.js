47a6d32ffaff1cba36c8b9e217118667b024f97f
Francis Kyle <kyle.francis@saint-benigne.fr>
2022-07-14 18:18:04 +0200

    acces aux images

--- Site/Js/index.js
+++ Site/Js/index.js
@@ -1,48 +1,11 @@
-// Liste des personnages 
-const characterSelectionList = document.querySelector('#characterSelectionList');
-characterSelectionList.addEventListener('click', DisplayCharacter);
-
-// Personnage 
-const character = document.querySelector('#character');
-
-
-function DisplayCharacter(){
-    const characterSelected = characterSelectionList.options[characterSelectionList.selectedIndex].id;
-    character.src = characterSelected;
+function chooseAnimation(){
+    let animationSelected = animationList.options[animationList.selectedIndex].innerHTML;
+    animation.src = 'Image/' + animationSelected;
 }
 
+const animation = document.querySelector('#animation');
 
-function GetCharacters(){
-    // Requête pour récupérer les images
-    // Avec l'autorisation 
-    fetch('https://api.github.com/repos/nuyare/dream1day/contents/Site/Image',{
-        headers: {
-            'Authorization' : 'token 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'
-        }
-    })
-    // Recevoir que le contenu JSON 
-    .then(response => {
-        return response.json();
-    })
-    // Récupérer que le contenu JSON 
-    .then(json => {
-        const characters = json;
-        if (characterSelectionList != null) {
-            characters.forEach(character => 
-                {   
-                    // Option 
-                    const characterOption = document.createElement('option');
-                    characterOption.id = character.download_url;
-                    characterOption.innerHTML = character.name;
+const animationList = document.querySelector('#characterSelectionList');
+animationList.addEventListener('click', chooseAnimation);
 
-                    // On ajoute l'option à la liste
-                    characterSelectionList.appendChild(characterOption);
-
-                });
-        }
-    });
-
-}
-
-
-GetCharacters();
+chooseAnimation();
